d14528a978ea75ac0fb9478f3b740c81d1876670
RinFroster <masterofblade69@gmail.com>
2020-11-28 23:03:58 +0700

    upload btJavascript

---
+++ js/main.js
@@ -0,0 +1,64 @@
+document
+  .getElementsByClassName("contact100-form-btn")[0]
+  .addEventListener("click", function () {
+    document.getElementsByClassName("uberSUV").checked;
+    document.getElementsByClassName("uberBlack").checked;
+    var txtKm = document.getElementsByClassName("input100")[0].value;
+    var txtTime = document.getElementsByClassName("input100")[1].value;
+    function checkMoney(
+      valueFirst,
+      valueSecond,
+      valueThird,
+      valueWait,
+      totalCheck
+    ) {
+      if (txtKm <= 1 && txtKm > 0) {
+        totalCheck = valueFirst + valueWait;
+        console.log(totalCheck + "vnđ");
+      } else if (txtKm > 1 && txtKm <= 20) {
+        totalCheck = valueSecond + valueFirst + valueWait;
+        console.log(totalCheck + "vnđ");
+      } else if (txtKm > 21) {
+        totalCheck = valueThird + valueWait;
+        console.log(totalCheck + "vnđ");
+      }
+    }
+    if (
+      document.getElementById("uberX").checked
+    ) {
+      checkMoney(
+        8000,
+        12000 * (txtKm - 1),
+        8000 + 12000 * 19 + 10000 * (txtKm - 20),
+        txtTime * 2000,
+        0
+      );
+    } else if (
+      document.getElementById("uberSUV").checked
+    ) {
+      checkMoney(
+        9000,
+        14000 * (txtKm - 1),
+        9000 + 14000 * 19 + 12000 * (txtKm - 20),
+        txtTime * 3000,
+        0
+      );
+    } else if (
+      document.getElementById("uberBlack").checked
+    ) {
+      checkMoney(
+        10000,
+        16000 * (txtKm - 1),
+        10000 + 16000 * 19 + 14000 * (txtKm - 20),
+        txtTime * 4000,
+        0
+      );
+    } else {
+      var nullAlert = alert("Vui lòng chọn loại xe");
+      console.log(nullAlert);
+    }
+    if(txtKm == 0){
+      var nullInfoAlert = alert("Vui lòng nhập số km và thời gian chờ");
+      console.log(nullInfoAlert);
+    }
+  });
